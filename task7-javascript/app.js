16f3a8a4a06f120b479dc45dc5bf7824569bfded
owaissyed65 <owaissyed65@gmail.com>
2023-04-04 17:38:50 +0500

    javascript

---
+++ task7-javascript/app.js
@@ -0,0 +1,26 @@
+// this is comment
+let a = 2
+var c = 10
+const d = 5
+// int float boolean mainly null undefined
+// operators +-*/%
+// these are operator
+let string = c + "hello"
+let string1 = c - "3"
+console.log(string1);
+c == 5;
+a < 4
+a > 1
+if (a == 2) {
+    console.log('i am true')
+} else {
+    console.log('i m not true');
+}
+for (let index = 0; index < 4; index++) {
+    console.log(index);
+}
+function greet(name) {
+    console.log(name);
+}
+greet('owais')
+// scope is all about block global and function scope
